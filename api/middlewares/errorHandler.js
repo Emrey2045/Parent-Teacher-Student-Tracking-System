4f58a223de5b4a7882579be61da520cbf9693758
Emrey2045 <136327360+Emrey2045@users.noreply.github.com>
2025-12-04 19:30:45 +0300

    Remove emoji from error logging message

--- api/middlewares/errorHandler.js
+++ api/middlewares/errorHandler.js
@@ -1,6 +1,6 @@
 // middlewares/errorHandler.js
 export function errorHandler(err, req, res, next) {
-    console.error("🔥 HATA:", err.message);
+    console.error(" HATA:", err.message);
 
     res.status(err.status || 500).json({
         success: false,
