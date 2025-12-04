402cdc8c6a1f68aeefeee27a3493eb6575726cc4
Emrey2045 <136327360+Emrey2045@users.noreply.github.com>
2025-12-04 19:31:24 +0300

    Fix comments formatting in config.js

--- api/config/config.js
+++ api/config/config.js
@@ -4,16 +4,16 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const CONFIG = {
-    // 🌍 Ortam değişkenleri
+    //  Ortam değişkenleri
     env: process.env.NODE_ENV || "development",
     port: process.env.PORT || 5000,
     appName: "SmartQ API",
 
-    // 🧩 Veritabanı bağlantısı
+    //  Veritabanı bağlantısı
     databaseUrl: process.env.DATABASE_URL,
     logLevel: process.env.LOG_LEVEL || "info",
 
-    // 🔐 JWT ayarları
+    //  JWT ayarları
     jwtSecret: process.env.JWT_SECRET || "fallback_secret_key",
     jwtRefreshSecret: process.env.JWT_REFRESH_SECRET || "fallback_refresh_key",
 };
